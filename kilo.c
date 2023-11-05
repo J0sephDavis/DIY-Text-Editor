c2aa88c278fe109618cac3d489af86f5b684a4b6
Joseph Davis <josephdavisjoey@gmail.com>
2023-11-05 11:58:36 -0500

    printing output of each keypress

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -9,18 +11,24 @@ void disableRawMode() {
 }
 
 void enableRawMode() {
-	tcgetattr(STDIN_FILENO, &original_termios); //get the origin termios & save it
-	atexit(disableRawMode); //ensures that disableRawMode() is called on application exit()
+	tcgetattr(STDIN_FILENO, &original_termios); 	//get the origin termios & save it
+	atexit(disableRawMode); 			//ensures that disableRawMode() is called on application exit()
 
-	struct termios raw = original_termios; //struct that holds the termio setup for raw input
-	raw.c_lflag &= ~(ECHO); // a "bitwise AND" assignment of a "bitewise NOT" of ECHO. Should disable ECHOing of user input
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); //sets the state of the FD to the new struct we've created
-						  //TCSAFLUSH - only applies changes after all pending output is written, discards unread input
+	struct termios raw = original_termios; 		//struct that holds the termio setup for raw input
+	//c_lflag is the "local flag"
+	raw.c_lflag &= ~(ECHO | ICANON); 		//disable ECHOing & canonical mode(user-input sending on enter)
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 	//sets the state of the FD to the new struct we've created
+						  	//TCSAFLUSH - only applies changes after all pending output is written, discards unread input
 }
 
 int main() {
 	enableRawMode();
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) 		//tests whether the character is a ctrl character or not. i.e, if it is a non-printable character
+			printf("%d\n", c);
+		else
+			printf("%d ('%c')\n",c,c);
+	};
 	return 0;
 }
