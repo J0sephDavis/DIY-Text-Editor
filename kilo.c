2a263f02a3499b6967656c7741a37201ede7acf3
Joseph Davis <josephdavisjoey@gmail.com>
2023-11-05 11:52:48 -0500

    now resets termio on exit()

--- kilo.c
+++ kilo.c
@@ -1,11 +1,21 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios original_termios; //the original state of the user's termio
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios); //sets the users termio back to how it was
+}
+
 void enableRawMode() {
-	struct termios raw; //struct that holds current attributes
-	tcgetattr(STDIN_FILENO, &raw); //load the current attributes into the struct
+	tcgetattr(STDIN_FILENO, &original_termios); //get the origin termios & save it
+	atexit(disableRawMode); //ensures that disableRawMode() is called on application exit()
+
+	struct termios raw = original_termios; //struct that holds the termio setup for raw input
 	raw.c_lflag &= ~(ECHO); // a "bitwise AND" assignment of a "bitewise NOT" of ECHO. Should disable ECHOing of user input
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); //sets the state of the FD to the new struct we've created
+						  //TCSAFLUSH - only applies changes after all pending output is written, discards unread input
 }
 
 int main() {
