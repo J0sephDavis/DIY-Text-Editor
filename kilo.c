cbae6b2f6b2b4264d4197ce908d5fccbd07d8367
Joseph Davis <josephdavisjoey@gmail.com>
2023-11-05 22:02:10 -0500

    updated the behavor of page up & down

--- kilo.c
+++ kilo.c
@@ -412,6 +412,12 @@ void editorProcessKeypress() {
 		case PAGE_UP:
 		case PAGE_DOWN:
 			{
+				if (c == PAGE_UP) {
+					E.cy = E.row_off;
+				} else if (c == PAGE_DOWN) {
+					E.cy = E.row_off + E.screenrows - 1;
+					if (E.cy > E.numrows) E.cy = E.numrows;
+				}
 				int times = E.screenrows;
 				while (times--)
 					editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
